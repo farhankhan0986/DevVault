3713ff4fee7d090eae3cab6942ea9c0d241b4403
Farhan Abid <farhankhan080304@gmail.com>
2026-03-17 20:20:45 +0530

    sentinel guard added

--- app/components/AllProjects.jsx
+++ app/components/AllProjects.jsx
@@ -45,25 +45,30 @@ export default function AllProjects() {
   },
 
   {
-    id: 3,
-    title: "Sentinel Guard",
-    description:
-      "A multi-tenant API firewall and security platform built with Next.js middleware. It provides request interception, rate limiting, attack detection, logging, and an admin dashboard for real-time security insights.",
-    image: "/images/sentinal.png",
-    tech: [
-      "Next.js",
-      "Node.js",
-      "Middleware",
-      "MongoDB",
-      "JWT",
-      "Security Engineering",
-    ],
-    github: "https://github.com/farhankhan0986/Sentinel-Guard",
-    status: "COMPLETED",
-    highlight: "Real-world API Firewall & Security Platform",
-    href: "https://sentinel-guard-eb9e.vercel.app/",
-    slug: "sentinel-guard",
-  },
+  id: 3,
+  title: "Sentinel Guard",
+  description:
+    "A full-stack website security monitoring platform that helps users protect and observe incoming API traffic for their websites. It allows users to create an account, register their website, generate a unique API key, inspect request activity through a professional dashboard, apply request filtering and rate limiting, detect suspicious traffic patterns, and monitor blocked requests with real-time analytics.",
+  image: "/images/sentinal.png",
+  tech: [
+    "Next.js",
+    "React",
+    "MongoDB",
+    "Mongoose",
+    "JavaScript",
+    "Tailwind CSS",
+    "JWT Authentication",
+    "REST API",
+    "Middleware",
+    "Recharts",
+  ],
+  github: "https://github.com/farhankhan0986/Sentinel-Guard",
+  status: "COMPLETED",
+  highlight:
+    "Middleware-based traffic monitoring",
+  href: "https://sentinel-guard-eb9e.vercel.app/",
+  slug: "sentinel-guard",
+},
 
   {
     id: 1,
